68306ef25090b4e4b4825aee32cafe289a36bd6d
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2022-06-27 17:10:32 +0900

    Modify to Load Forgery Target Signature Image
    Skilled 위조 서명 연습 및 등록 시 Image Dir 내에서 위조할 대상의 서명 이미지를 불러오도록

--- app/src/main/java/com/me/handwrittensignature/ForgerySign_Skilled_Practice.java
+++ app/src/main/java/com/me/handwrittensignature/ForgerySign_Skilled_Practice.java
@@ -45,23 +45,23 @@ public class ForgerySign_Skilled_Practice extends AppCompatActivity {
     ImageView iv;
     public static String name;
     public static String targetName;
-    public static String targetFile;
 
-    private final String rootPath = Environment.getExternalStorageDirectory() + "/Movies/Signature_ver_Record/";
-    private String targetPath;   // 위조할 대상의 디렉토리
-    private String targetSignature;   // 위조 대상의 실제 서명 디렉토리 중 랜덤 선택된 서명
-    private String targetSignaturePath;   // 위조 대상의 실제 서명 디렉토리 중 랜덤 선택된 서명 이미지 경로 => 기록 부분으로 넘겨주기
+    private final String videoRootPath = Environment.getExternalStorageDirectory() + "/Movies/Signature_ver_Record/";
+    private final String imageRootPath = Environment.getExternalStorageDirectory() + "/Pictures/Signature_ver_Record/";
+    private String targetImageDirPath;   // 위조할 대상의 디렉토리
+    private String targetImageSignature;   // 위조 대상의 실제 서명 디렉토리 중 랜덤 선택된 서명
+    private String targetImageSignaturePath;   // 위조 대상의 실제 서명 디렉토리 중 랜덤 선택된 서명 이미지 경로 => 기록 부분으로 넘겨주기
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.skilled_forgery_sign_practice);
 
-        TextView modeText = (TextView)findViewById(R.id.modeText);
-        Button loadButton = (Button)findViewById(R.id.loadButton);
-        Button startButton = (Button)findViewById(R.id.button_start);
-        Button restartButton = (Button)findViewById(R.id.button_restart);
-        Button endButton = (Button)findViewById(R.id.button_end);
+        modeText = findViewById(R.id.modeText);
+        Button loadButton = findViewById(R.id.loadButton);
+        Button startButton = findViewById(R.id.button_start);
+        Button restartButton = findViewById(R.id.button_restart);
+        Button endButton = findViewById(R.id.button_end);
 
         modeText.setVisibility(View.VISIBLE);
 
@@ -98,14 +98,10 @@ public class ForgerySign_Skilled_Practice extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                //불러오기 버튼 숨기기
-                loadButton.setEnabled(false);
 
                 try {
-                    loadTargetSignature();
-                    File storageDir = new File(targetSignaturePath);
-                    String loadImgName = targetFile;
-                    File file = new File(storageDir, loadImgName);
+                    targetImageSignaturePath = loadTargetSignature();
+                    File file = new File(targetImageSignaturePath);
                     Bitmap bitmap = BitmapFactory.decodeStream(new FileInputStream(file));
                     iv.setImageBitmap(bitmap);
 
@@ -113,19 +109,21 @@ public class ForgerySign_Skilled_Practice extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "이미지 로드 실패", Toast.LENGTH_SHORT).show();
                 }
 
+                loadButton.setEnabled(false);
+
             }
         });
 
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                signaturePad.setEnabled(true);   // 서명 패드 활성화
+                signaturePad.setEnabled(true);
 
                 loadButton.setVisibility(View.GONE);
-                startButton.setVisibility(View.GONE);   // 시작 버튼 숨기기
-                restartButton.setVisibility(View.VISIBLE);   // 초기화 버튼 나타나게
-                endButton.setVisibility(View.VISIBLE);   // 종료 버튼 나타나게
-                startButton.setEnabled(false);   // 시작 버튼 비활성화
+                startButton.setVisibility(View.GONE);
+                restartButton.setVisibility(View.VISIBLE);
+                endButton.setVisibility(View.VISIBLE);
+                startButton.setEnabled(false);
             }
         });
 
@@ -141,12 +139,12 @@ public class ForgerySign_Skilled_Practice extends AppCompatActivity {
         endButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Skilled 연습 페이지 종료 시  skilled 위조 서명  등록 화면으로
+                // Skilled 연습 페이지 종료 시 skilled 위조 서명 등록 화면으로
                 Intent intent2 = new Intent(getApplicationContext(), ForgerySign_Skilled.class);
                 intent2.putExtra("text", name);
                 intent2.putExtra("targetName", targetName);
-                intent2.putExtra("targetPath", targetPath);
-                intent2.putExtra("targetSignaturePath", targetSignaturePath);
+                intent2.putExtra("targetPath", targetImageDirPath);
+                intent2.putExtra("targetSignaturePath", targetImageSignaturePath);
                 startActivity(intent2);
             }
         });
@@ -156,42 +154,43 @@ public class ForgerySign_Skilled_Practice extends AppCompatActivity {
     /**
      * 위조 대상의 서명 데이터를 가져올 메서드 - targetFile = 위조 대상의 서명 이미지 경로(String)
      */
-    private void loadTargetSignature() {
-        File directory = new File(rootPath);
-        File[] files = directory.listFiles();   // ~/Signature_ver2 디렉토리 내 파일 목록
-        List<String> filesDirList = new ArrayList<>();
+    public String loadTargetSignature() {
+        File directory = new File(imageRootPath);
+        File[] files = directory.listFiles();   // 디렉토리 내 파일 목록
+        List<String> imageFilesDirList = new ArrayList<>();
 
         for (int i = 0; i < files.length; i++) {
-            filesDirList.add(files[i].getName());
+            imageFilesDirList.add(files[i].getName());
         }
 
-        filesDirList.remove(name);   // 본인의 디렉토리(서명은)는 위조 대상에서 제외
+        imageFilesDirList.remove(name);   // 본인의 디렉토리(서명은)는 위조 대상에서 제외
 
         // 위조할 타겟 대상의 디렉토리 랜덤 선택
-        int idx1 = new Random().nextInt(filesDirList.size());
-        targetName = filesDirList.get(idx1);
+        int idx1 = new Random().nextInt(imageFilesDirList.size());
+        targetName = imageFilesDirList.get(idx1);
 
         // TODO 위조할 타켓 대상의 디렉토리 내 서명 선택 - 각 서명 디렉토리 이름 중 unskilled or skilled 문자열 미포함 디렉토리 선택
-        targetPath = rootPath + targetName;
-        File targetPathFiles = new File(targetPath);
-        File[] targetPathFileList = targetPathFiles.listFiles();   // 위조할 타켓 대상 디렉토리 내의 목록
+        targetImageDirPath = imageRootPath + targetName;
+        File targetImagePathFiles = new File(targetImageDirPath);
+        File[] targetImagePathFileList = targetImagePathFiles.listFiles();   // 위조할 타켓 대상 디렉토리 내의 목록
         List<String> targetPathFolderList = new ArrayList<>();
 
-        for (int i = 0; i < targetPathFileList.length; i++) {
-            targetPathFolderList.add(targetPathFileList[i].getName());
+        for (int i = 0; i < targetImagePathFileList.length; i++) {
+            targetPathFolderList.add(targetImagePathFileList[i].getName());
         }
 
         for (int i = 0; i < targetPathFolderList.size(); i++) {
-            if (targetPathFolderList.get(i).contains("unskilled") || targetPathFolderList.get(i).contains("skilled") ||targetPathFolderList.get(i).contains("mp4") ) {
+            if (targetPathFolderList.get(i).contains("unskilled") || targetPathFolderList.get(i).contains("skilled") || targetPathFolderList.get(i).contains("mp4") ) {
                 targetPathFolderList.remove(targetPathFolderList.get(i));   // 위조 대상의 실제 서명 이미지들만 남기기
             }
         }
 
         // TODO 위조할 타겟 대상의 디렉토리 내 서명 - unskilled or skilled 문자열 미포함 -> png 파일 중 랜덤 선택
         int idx2 = new Random().nextInt(targetPathFolderList.size());
-        targetSignature = targetPathFolderList.get(idx2);
-        targetSignaturePath = targetPath + "/" + targetSignature;
+        targetImageSignature = targetPathFolderList.get(idx2);
+        targetImageSignaturePath = targetImageDirPath + "/" + targetImageSignature;
 
+        return targetImageSignaturePath;
     }
 
 }
